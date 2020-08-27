c7457e22075d7b08244adbe2930d04f2fb034249
Shichimin <35617411+Shichimin@users.noreply.github.com>
2020-08-27 13:16:08 +0900

    ActiveHashを継承したCategoryモデルとPrefectureモデルを作成

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,9 @@
-class Category < ApplicationRecord
+class Category < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: 'グルメ' },
+    { id: 3, name: '映画・舞台' },
+    { id: 4, name: 'スポーツ・レジャー' },
+    { id: 5, name: 'その他' }
+  ]
 end
