1572cc07a13f3a9943177ffe108bb4a2e7bd64b4
Shichimin <35617411+Shichimin@users.noreply.github.com>
2020-08-30 16:39:28 +0900

    相手とのチャットルームがすでに存在すれば遷移、存在しなければ作成

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,4 +2,21 @@ class RoomsController < ApplicationController
   def new
     @room = Room.new
   end
+
+  def create
+    @room = Room.create
+    @room_user1 = RoomUser.create(room_id: @room.id, user_id: current_user.id)
+    @room_user2 = RoomUser.create(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def room_params
+    params.require(:room_user).permit(:user_id, :room_id).merge(room_id: @room.id)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,31 @@
 class UsersController < ApplicationController
+  before_action :move_to_login
+
   def show
     @user = User.find_by(id: params[:id])
+
+    @room_user_by_current_user = RoomUser.where(user_id: current_user.id)
+    @room_user_by_user = RoomUser.where(user_id: @user.id)
+
+    @room_user_by_current_user.each do |cu|
+      @room_user_by_user.each do |u|
+        if cu.room_id == u.room_id then
+          @is_room = true
+          @room_id = cu.room_id
+        end
+      end
+    end
+    unless @is_room
+      @room = Room.new
+      @room_user = RoomUser.new
+    end
+  end
+
+  private
+
+  def move_to_login
+    unless user_signed_in?
+      redirect_to user_session_path
+    end
   end
 end
