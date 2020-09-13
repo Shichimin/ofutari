ec3359b0a54967c94a2b3bb6e995c77d02bb7b26
Shichimin <35617411+Shichimin@users.noreply.github.com>
2020-09-13 17:15:41 +0900

    [update]新規登録ページのレイアウトを改修

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -17,15 +17,15 @@ consumer.subscriptions.create("RoomChannel", {
     const messages = document.getElementById('container-inner');
     const newMessage = document.getElementById('send_message');
     const html = `
-    <ul class="chat-messages">
-      <li class='right-side'>
-        <div class='user-pic'>
-          <img src='${url}.png'>
-        </div>
-        <div class='message-text'>
-          ${data.content.content}
-        </div>
-      </li>
+      <ul class="chat-messages">
+        <li class='right-side'>
+          <div class='user-pic'>
+            <img src='${url}.png'>
+          </div>
+          <div class='message-text'>
+            ${data.content.content}
+          </div>
+        </li>
       </ul>`;
     messages.insertAdjacentHTML('beforeend', html);
     const obj = document.getElementById("container-inner");
